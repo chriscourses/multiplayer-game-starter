82e90d0dbb8a2f16a8ccccbb40ac1c66a681e8b0
shivamjagdishsingh <shivamjagdishsingh@gmail.com>
2023-08-19 14:54:30 +0530

    Clean code and add socket io

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+
+// Socket.io requires http server and not express server
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const port = 3000
 
 app.use(express.static('public'))
@@ -8,6 +15,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
